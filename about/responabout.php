842ae19af052b4ceffc78f934b450c46495df829
Sana SK <118280128+Sana842@users.noreply.github.com>
2024-07-28 20:59:23 +0530

    Add files via upload

---
+++ about/responabout.php
@@ -0,0 +1,461 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>iDESIGN - Interior Design HTML Template</title>
+	<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<meta content="Free HTML Templates" name="keywords">
+	<meta content="Free HTML Templates" name="description">
+
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" rel="stylesheet">
+
+	<!-- Customized Bootstrap Stylesheet -->
+	<link href="responabout.css" rel="stylesheet">
+	<link href="footer.css" rel="stylesheet">
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+	<!-- Bootstrap 5 JavaScript Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+
+
+
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" rel="stylesheet">
+
+	<!-- Bootstrap 5 CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+	<link href="footer.css" rel="stylesheet">
+	<!-- Font Awesome 5 CSS -->
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.13.0/css/all.css">
+
+	<!-- Customized Bootstrap Stylesheet -->
+
+</head>
+
+<body>
+	<?php
+	include("../navbar/navbar.php");
+	?>
+	<br>
+	<br>
+
+	<!-- Banner section -->
+	<div class="main1">
+		<div id="abs" class="col-lg text-center" data-aos-delay="200">
+			<h1 id data-aos="zoom-out">Learn more about us !</h1>
+			<h6 data-aos="zoom-out">We provide the best quality services and applications. We help you design beautiful spaces!</h6>
+			<!-- <button class="b2">Explore our Services</button> -->
+		</div>
+	</div>
+
+	<!-- Section 1 -->
+	<div class="row  align-items-center" id="main2">
+
+
+		<div class="col-lg-6 mb-3" id="mm1" data-aos="fade-up" data-aos-delay="100" data-aos="fade-right">
+			<img src="./img/002.jpg" alt="Image" class="img-fluid">
+		</div>
+
+		<div class="col-lg-6 text-center " id="ss1" data-aos-delay="200" data-aos="fade-left">
+			<h1 style="color:#00375B;">Expertly crafted designs for all spaces, reflecting your style</h1>
+			<p>Our expert team meticulously crafts designs tailored to every space, ensuring they reflect your unique style.
+				From cozy bedrooms to functional kitchens, we blend creativity and functionality to maximize comfort and practicality.
+				Trust us for an exceptional interior experience that transforms your home into a reflection of your personality.
+			</p>
+			<!-- <div class="btn1">
+				<button class="b2">Check our services</button>
+			</div> -->
+			<!-- <div class="icon">
+				<img src="./img/icon.jpg" alt="">
+			</div> -->
+		</div>
+
+	</div>
+
+	<!-- team Section -->
+
+	<section id="team">
+		<div class="container" id="tt1">
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+					<div class="sec-heading text-center">
+						<h6>Our Team Members</h6>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="testimonial-box" data-aos="zoom-in-up">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="team-slider owl-carousel">
+							<div class="single-box text-center">
+								<div class="img-area"><img alt="" class="img-fluid move-animation" src="./img/team-1.jpg"></div>
+								<div class="info-area">
+									<h4>Person's Name</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, ullam.</p>
+									<!-- <button class="b2">Learn More</button> -->
+
+								</div>
+							</div>
+							<div class="single-box text-center">
+								<div class="img-area"><img alt="" class="img-fluid move-animation" src="./img/team-2.jpg"></div>
+								<div class="info-area">
+									<h4>Person's Name</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, ullam.</p>
+									<!-- <button class="b2">Learn More</button>
+									 -->
+								</div>
+							</div>
+							<div class="single-box text-center">
+								<div class="img-area"><img alt="" class="img-fluid move-animation" src="./img/team-3.jpg"></div>
+								<div class="info-area">
+									<h4>Person's Name</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, ullam.</p>
+									<!-- <button class="b2">Learn More</button> -->
+
+								</div>
+							</div>
+							<div class="single-box text-center">
+								<div class="img-area"><img alt="" class="img-fluid move-animation" src="./img/team-4.jpg"></div>
+								<div class="info-area">
+									<h4>Person's Name</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, ullam.</p>
+									<!-- <button class="b2">Learn More</button> -->
+
+								</div>
+							</div>
+							<div class="single-box text-center">
+								<div class="img-area"><img alt="" class="img-fluid move-animation" src="./img/team-1.jpg"></div>
+								<div class="info-area">
+									<h4>Person's Name</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, ullam.</p>
+									<!-- <button class="b2">Learn More</button> -->
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<!-- testmonial -->
+	<section class="client44 pt-3 pb-5">
+		<div class="container">
+			<div class="row text-center">
+				<div class="col-12">
+					<h1 class="display-3 fw-bold text-white">Testimonials</h1>
+					<hr class="bg-white mb-4 mt-0 d-inline-block mx-auto" style="width: 100px; height:3px;">
+					<p class="p-text text-white">What our clients are saying</p>
+				</div>
+			</div>
+			<div class="outer-size22">
+				<div class="size22">
+					<div class="row align-items-md-center text-white">
+						<div class="col-lg-12 col-md-12 col-sm-12">
+							<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+								<!-- Wrapper for slides -->
+								<div class="carousel-inner">
+									<div class="carousel-item active">
+										<div class="row p-4">
+
+											<div class="row">
+												<div class="col-sm-2 pt-3">
+													<img src="./img/testimonial-1.jpg" class="rounded-circle img-responsive img-fluid">
+												</div>
+												<div class="col-sm-10">
+													<h4><strong>Sunaina Samuel</strong></h4>
+													<p class="testimonial_subtitle"><span>Associate Software Engineer</span>
+
+														<span>CodeHim</span>
+													</p>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="carousel-item">
+										<div class="row p-4">
+
+											<div class="row">
+												<div class="col-sm-2 pt-4">
+													<img src="./img/testimonial-2.jpg" class="rounded-circle img-responsive img-fluid">
+												</div>
+												<div class="col-sm-10">
+													<h4><strong>Esther Zawadi</strong></h4>
+													<p class="testimonial_subtitle"><span>
+															digital strategist</span>
+
+														<span>Vaxa digital</span>
+													</p>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="carousel-item">
+										<div class="row p-4">
+
+											<div class="row text-lg-start">
+												<div class="col-sm-2 pt-4 align-items-center">
+													<img src="./img/testimonial-1.jpg" class="rounded-circle img-responsive img-fluid">
+												</div>
+												<div class="col-sm-10">
+													<h4><strong>Patrick muriungi</strong></h4>
+													<p class="testimonial_subtitle"><span>Web Technologist</span>
+
+														<span>Vaxa digital</span>
+													</p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="controls center-button-div">
+								<a class="left fa fa-chevron-left text-white btn btn btn-outline-light" href="#carouselExampleCaptions" data-bs-slide="prev"></a>
+								<a class="right fa fa-chevron-right text-white btn btn btn-outline-light" href="#carouselExampleCaptions" data-bs-slide="next"></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Number  -->
+
+
+	<div class="row55">
+		<div class="column55">
+			<div class="card44">
+				<p><i class="fa fa-user"></i></p>
+				<h3 data-aos="zoom-in-up">11+</h3>
+				<p data-aos="zoom-in-up"> Partners</p>
+			</div>
+		</div>
+
+		<div class="column55">
+			<div class="card44">
+				<p><i class="fa fa-check"></i></p>
+				<h3 data-aos="zoom-in-up">55+</h3>
+				<p data-aos="zoom-in-up">Projects</p>
+			</div>
+		</div>
+
+		<div class="column55">
+			<div class="card44">
+				<p><i class="fa fa-smile-o"></i></p>
+				<h3 data-aos="zoom-in-up">100+</h3>
+				<p data-aos="zoom-in-up">Happy Clients</p>
+			</div>
+		</div>
+
+		<div class="column55">
+			<div class="card44">
+				<p><i class="fa fa-coffee"></i></p>
+				<h3 data-aos="zoom-in-up">100+</h3>
+				<p data-aos="zoom-in-up">Meetings</p>
+			</div>
+		</div>
+	</div>
+
+
+	<!-- features section -->
+
+
+	<div class="gallery-heading">
+		<h1 id="feat">Our Features</h1>
+	</div>
+	<div class="row66">
+		<div class="column33" data-aos="fade-right">
+			<div class="card33">
+				<p> <i style="font-size:30px;" class="fa fa-solid fa-reply"></i></p>
+
+				<p>Faster Response</p>
+			</div>
+		</div>
+
+		<div class="column33">
+			<div class="card33" data-aos="fade-right">
+				<p> <i style="font-size:30px;" class="fa fa-solid fa-reply"></i></p>
+
+				<p>Best Designs</p>
+			</div>
+		</div>
+
+		<div class="column33">
+			<div class="card33" data-aos="fade-down">
+				<p> <i style="font-size:30px;" class="fa fa-solid fa-reply"></i></p>
+
+				<p>Faster Working</p>
+			</div>
+		</div>
+
+
+		<div class="column33">
+			<div class="card33" data-aos="fade-left">
+				<p><i style="font-size:30px;" class="fa fa-solid fa-camera"></i></p>
+
+				<p>Variation Available</p>
+			</div>
+		</div>
+
+
+		<div class="column33">
+			<div class="card33" data-aos="fade-left">
+				<p> <i style="font-size:30px;" class="fa fa-solid fa-reply"></i></p>
+
+				<p>Customizable Patterns</p>
+			</div>
+		</div>
+	</div>
+
+	<?php
+	//include("../footer11/footer.php");
+	?>
+
+	<footer class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="footer-col">
+					<h4>Elite Custom Enterprises</h4>
+					<ul>
+						<li><a href="#"> We are providing</a></li>
+						<li><a href="#">variouse services to make</a></li>
+						<li><a href="#">your spaces ready for</a></li>
+						<li><a href="#">good startup !</a></li>
+					</ul>
+				</div>
+				<div class="footer-col">
+					<h4>Quick Links</h4>
+					<ul>
+						<li><a href="../home/index.php">Home</a></li>
+						<li><a href="../about/responabout.php">About Us</a></li>
+						<li><a href="../home/#service">Service</a></li>
+						<li><a href="../gallery/gallery.php">Gallery</a></li>
+						<li><a href="../contact/contact.php">Contact Us</a></li>
+
+					</ul>
+				</div>
+				<div class="footer-col">
+					<h4>Contact</h4>
+					<ul id="">
+						<li>
+							<a href="#"> <i class="fa-solid fa-location-dot"></i> 123 Street, New York, USA</a>
+						</li>
+						<li><a href="#"> <i class="fa-solid fa-phone"></i> + 0123456790</a></li>
+						<li><a href="#"> <i class="fa-solid fa-envelope"></i> interior@gmail.com</a></li>
+
+						<div class="social-links">
+							<a href="#"><i class="fab fa-facebook-f"></i></a>
+							<a href="#"><i class="fab fa-twitter"></i></a>
+							<a href="#"><i class="fab fa-instagram"></i></a>
+							<a href="#"><i class="fab fa-linkedin-in"></i></a>
+						</div>
+					</ul>
+
+
+				</div>
+				<div class="footer-col">
+					<h4>Gallery</h4>
+
+					<div class="col">
+						<div class="row22">
+							<div class="column22">
+								<img src="./img11/foot1.jpg" alt="Snow" style="width:100%">
+							</div>
+							<div class="column22">
+								<img src="./img11/foot2.jpg" alt="Forest" style="width:100%">
+							</div>
+							<div class="column22">
+								<img src="./img11/foot3.jpg" alt="Mountains" style="width:100%">
+							</div>
+						</div>
+
+
+						<div class="column22">
+							<img src="./img11/foot4.jpg" alt="Snow" style="width:100%">
+						</div>
+						<div class="column22">
+							<img src="./img11/foot5.jpg" alt="Forest" style="width:100%">
+						</div>
+						<div class="column22">
+							<img src="./img11/foot6.jpg" alt="Mountains" style="width:100%">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<hr style="margin-top: 30px;background-color:white">
+		<div class="row2">
+			<p style="text-align: center;color:#fff; font-size:20px;margin-top:25px">&#169; All rights reserved</p>
+		</div>
+	</footer>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js">
+	</script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+	</script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js">
+	</script>
+	<script>
+		$('.team-slider').owlCarousel({
+			loop: true,
+			nav: false,
+			autoplay: true,
+			autoplayTimeout: 5000,
+			smartSpeed: 450,
+			margin: 20,
+			responsive: {
+				0: {
+					items: 1
+				},
+				768: {
+					items: 2
+				},
+				991: {
+					items: 3
+				},
+				1200: {
+					items: 3
+				},
+				1920: {
+					items: 3
+				}
+			}
+		});
+	</script>
+	<!-- aos data  -->
+	<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+	<script>
+		AOS.init({
+			offset: 300,
+			duration: 1000,
+		});
+	</script>
+	<!-- Form Section -->
+</body>
+
+</html>
+
+
+
+
+
+
+</body>
+
+</html>
